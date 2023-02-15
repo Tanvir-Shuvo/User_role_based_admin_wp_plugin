6ac1a0c911a27ba19a0e58f885fe1b5779e8a9f5
Tanvir-Shuvo <tanvirkouser.shuvo@gmail.com>
2023-02-15 22:27:51 +0600

    first commit

---
+++ user-based-admin.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name:       Custom Admin Panel - User Role Based Admin
+ * Plugin URI:        https://rownakh8.sg-host.com/
+ * Description:       User role based customizable admin panel
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Tanvir Shuvo
+ * Author URI:        https://rownakh8.sg-host.com/shop/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       user-based-admin
+ * Domain Path:       /languages
+ */
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
